f9e76c4a4e024383c2acc744368b267b843f2d7c
Marco Antonio <marcoantonio.f.junir@gmail.com>
2019-10-20 11:13:56 -0300

    ts for enviado

---
+++ alimentacao.front/src/app/enviado/enviado.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'enviado',
+  templateUrl: './enviado.component.html',
+  styleUrls: ['./enviado.component.css']
+})
+export class EnviadoComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+// import {Component} from '@angular/core';
+
+// /** @title Basic drawer */
+// @Component({
+//   selector: 'sidenav-drawer-overview-example',
+//   templateUrl: 'sidenav-drawer-overview-example.html',
+//   styleUrls: ['sidenav-drawer-overview-example.css'],
+// })
+// export class SidenavDrawerOverviewExample {}
+

d5ba69f2808e42dbc1f99779ffa987df67fc28a3
Marco Antonio <marcoantonio.f.junir@gmail.com>
2019-10-20 01:41:18 -0300

    inclusão do layout da aplicação.

--- alimentacao.front/src/app/app.module.ts
+++ alimentacao.front/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EnviadoComponent } from './enviado/enviado.component';
+import { PrevisaoComponent } from './previsao/previsao.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
+
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EnviadoComponent,
+    PrevisaoComponent,
+    RelatorioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
